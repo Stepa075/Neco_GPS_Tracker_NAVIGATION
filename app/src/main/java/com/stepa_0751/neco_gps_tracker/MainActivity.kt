0a1cc5158f8a0018a67df2b2c211d1174d7a3936
Stacionar-PC <stepa0751@gmail.com>
2023-01-27 23:42:34 +0200

    Lesson 2 complete

--- app/src/main/java/com/stepa_0751/neco_gps_tracker/MainActivity.kt
+++ app/src/main/java/com/stepa_0751/neco_gps_tracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.stepa_0751.neco_gps_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.stepa_0751.neco_gps_tracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onButtomNavClicks()
+    }
+    private fun onButtomNavClicks(){
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.id_home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                R.id.id_tracks ->Toast.makeText(this, "Tracks", Toast.LENGTH_SHORT).show()
+                R.id.id_settings ->Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
 }
