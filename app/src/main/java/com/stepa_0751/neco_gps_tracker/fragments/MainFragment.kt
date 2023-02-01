1c39166a50189ace86173e42d1e29554106bcfd4
Stacionar-PC <stepa0751@gmail.com>
2023-02-01 22:19:31 +0200

    Lesson 10 complete

--- app/src/main/java/com/stepa_0751/neco_gps_tracker/fragments/MainFragment.kt
+++ app/src/main/java/com/stepa_0751/neco_gps_tracker/fragments/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.stepa_0751.neco_gps_tracker.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.stepa_0751.neco_gps_tracker.R
 import com.stepa_0751.neco_gps_tracker.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
 
 
 class MainFragment : Fragment() {
@@ -16,8 +20,19 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // !!!Нужно настроить библиотеку OSMAndroid до инициализации фрагмента(разметки)!!!
+        settingsOsm()
+        //  Собственно именно здесь загружается разметка, в Inflate!!!
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    // конфиг для осм библиотеки
+    private fun settingsOsm(){
+        Configuration.getInstance().load(
+            activity as AppCompatActivity, //задаем доступ к оанным библиотеки только для нашего приложения
+        activity?.getSharedPreferences("osm-pref", Context.MODE_PRIVATE))
+        //конфигурируем юзер агента
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
     }
 
     companion object {
--- app/src/main/java/com/stepa_0751/neco_gps_tracker/utils/Extensions.kt
+++ app/src/main/java/com/stepa_0751/neco_gps_tracker/utils/Extensions.kt
@@ -1,11 +1,13 @@
 package com.stepa_0751.neco_gps_tracker.utils
 
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.stepa_0751.neco_gps_tracker.R
 
 fun Fragment.openFragment(f: Fragment){
+
     (activity as AppCompatActivity).supportFragmentManager
         .beginTransaction()
         .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out) // установка анимации
@@ -13,6 +15,16 @@ fun Fragment.openFragment(f: Fragment){
 }                                                // на activity_main.xml
 
 fun AppCompatActivity.openFragment(f: Fragment){
+    //  так мы проверяем и визуально отслеживаем какое имя фрагмента вызывается приложением
+    Log.d("MyLog", "Frag name: ${f.javaClass}")
+    //  в списке запущенных фрагментов у нас всегда только 0 при запуске или 1 при переходе от
+    //  фрагмента к фрагменту. Если список фрагментов не пуст...
+    if(supportFragmentManager.fragments.isNotEmpty()){
+        // и если название фрагмента равно открытому фрагменту - ничего не делаем
+        if(supportFragmentManager.fragments[0].javaClass == f.javaClass) return
+    }
+    // если список фрагментов пуст(старт программы) или не совпадает по названию с текущим
+    // выполняем замещение фрагмента вызываемым
     supportFragmentManager
         .beginTransaction()
         .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
